ad955dc7e01db8c8522aa0d4c5b6de8daa199b12
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-11-18 10:20:12 +0500

    updated

--- components/cards/featured/Resume.tsx
+++ components/cards/featured/Resume.tsx
@@ -8,14 +8,14 @@ import SocialCard from "@/components/Ui/Social";
 export default function Resume() {
   return (
     <Card className="md:h-full 2xl:h-full" >
-        <p className="text-lg xl:text-2xl font-medium text-primary-foreground">
+        <p className="leading-[160%] font-normal text-white text-[16px]">
         Backend Developer with over three Year of experience in designing, developing, and maintaining scalable backend systems. Proficient in Python, Node.js, and SQL, with a deep understanding of RESTful APIs, microservices architecture, and database management. Strong expertise in optimizing server performance and implementing robust security measures. Skilled in collaborating with cross-functional teams to deliver high-quality software solutions🚀. 
         </p>
         {/*Signature*/}
-        {/* <div>
+        <div>
             <Image
             src={Signature} alt="Hamza Ali"/>
-        </div> */}
+        </div>
         {/*Social and Resume Btn*/}
         <div className="flex items-center justify-between md:absolute md:bottom-6 md:left-6 md:w-[calc(100%-48px)]">
             {/*Social Btn*/}
